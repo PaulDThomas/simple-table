3a820a689e8b5e7753d928113da8f397e37f21bb
Paul Thomas <paul@asup.co.uk>
2023-05-19 09:28:08 +0100

    Add local storage mock

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { localStorageMock } from './__mocks__/localStorageMock';
+
+Object.defineProperty(window, 'localStorage', localStorageMock);
