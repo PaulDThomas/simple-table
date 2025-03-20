d5627bcb0bd7d739c9a3c5471511bd88183aa07e
Paul Thomas <paul@asup.co.uk>
2025-03-20 17:17:25 +0000

    Further CSS update, version bump

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
   simpleTableNullDate,
 } from "./functions/simpleTableNullDate";
 import { simpleTableSortFn } from "./functions/simpleTableSortFn";
+import "../global.d.ts";
 
 export * from "./components";
 export {
